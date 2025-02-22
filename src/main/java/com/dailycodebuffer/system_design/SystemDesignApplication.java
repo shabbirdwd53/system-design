28c96d65c1f238581ee181319d775cc25aa931d7
Shabbir Dawoodi <shabbirdwd53@gmail.com>
2025-02-22 10:52:52 +0530

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/dailycodebuffer/system_design/SystemDesignApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.system_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDesignApplication.class, args);
+	}
+
+}
